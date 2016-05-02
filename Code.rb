892b365b3dfef973b67daebfd7539db89e2333d8
ErnestasZ <ernestas.zemaitis@gmail.com>
2016-05-02 21:21:42 +0300

    papildymas

--- Code.rb
+++ Code.rb
@@ -1,9 +1,15 @@
 file = File.open("raides_text.txt", "r")
-text =  file.read.downcase.chomp
-puts text.count('a')
-a = text.split('')
+text =  file.read.downcase
+text = text.chomp
+#puts text.count('a')
+chars = text.split('').sort
+#puts chars
 
+hash_text = Hash.new(0)
+chars.each do |char|
+  hash_text[char] += 1
 
+end
 
-
+puts hash_text
 file.close
